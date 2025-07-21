22e25d94abe72bc6e08cc7e0adc97d80f424c69c
Arghya Ghoshal <rcbianarghya@gmail.com>
2025-07-21 17:23:54 +0530

    Update animation.js

--- css/js/animation.js
+++ css/js/animation.js
@@ -6,7 +6,7 @@ function summarizeText() {
     output.innerHTML = "⏳ Summarizing...";
     copyBtn.style.display = "none";
 
-    axios.post('https://text-summarizer-backend-8h7i.onrender.com', { text })
+    axios.post('https://text-summarizer-backend-8h7i.onrender.com/api/summarize', { text })
         .then(res => {
             output.innerHTML = res.data.summary;
             copyBtn.style.display = "block";
